351f5d2f023e716aa4de6590828e6be697fd5da3
Rebekah-Dayton <bekah060603@gmail.com>
2024-10-01 17:56:20 -0600

    created test files and wrote franchiseRouter tests

---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const app = require('../service');
+const { Role, DB } = require('../database/database.js');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  await DB.addUser(user);
+
+  user.password = 'toomanysecrets';
+  return user;
+}
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expect(testUserAuthToken).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+});
---
+++ src/routes/orderRouter.test.js
@@ -0,0 +1,67 @@
+const request = require('supertest');
+const app = require('../service');
+const { Role, DB } = require('../database/database.js');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+async function getAdminAuthToken() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  await DB.addUser(user);
+
+  user.password = 'toomanysecrets';
+  const loginRes = await request(app).put('/api/auth').send(user);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  return loginRes.body.token;
+}
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expect(testUserAuthToken).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+});
+
+test('get menu', async () => {
+    const getRes = await request(app).get('/api/order/menu');
+    expect(getRes.status).toBe(200);
+    expect(getRes.body).toBeDefined();
+});
+
+test('add menu item', async () => {
+    const token = await getAdminAuthToken();
+    const menuItem = { title: "Student", description: "No topping, no sauce, just carbs", image:"pizza9.png", price: 0.0001 };
+    
+    const addRes = await request(app).put('/api/order/menu').set('Authorization', `Bearer ${token}`).send(menuItem);
+    expect(addRes.status).toBe(200);
+    expect(addRes.body).toBeDefined();
+});
+
+
+test('fail add menu item', async () => {
+    const menuItem = { title: randomName(), description: "No topping, no sauce, just carbs", image:"pizza9.png", price: 0.0001 };
+    
+    const addRes = await request(app).put('/api/order/menu').set('Authorization', `Bearer ${testUserAuthToken}`).send(menuItem);
+    expect(addRes.status).toBe(403);
+    expect(addRes.body).toBeDefined();
+});
+
+test('get user orders', async () => {
+    const userOrder = {franchiseId: 1, storeId: 1, items:[{ menuId: 1, description: randomName(), price: 0.05 }]}
+
+    const addRes = await request(app).post('/api/order').set('Authorization', `Bearer ${testUserAuthToken}`).send(userOrder);
+    expect(addRes.status).toBe(200);
+    expect(addRes.body.jwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+});
+
+test('create order', async () => {
+    // TODO '/api/order'
+});
