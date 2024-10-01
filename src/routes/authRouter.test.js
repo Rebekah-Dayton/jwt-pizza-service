5768dabb8c6a97c519d64cf7e089e1eb87889256
Rebekah-Dayton <bekah060603@gmail.com>
2024-10-01 13:55:22 -0600

    created authRouter tests for register function

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -4,6 +4,23 @@ const app = require('../service');
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+const { Role, DB } = require('../database/database.js');
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  await DB.addUser(user);
+
+  user.password = 'toomanysecrets';
+  return user;
+}
+
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
@@ -19,4 +36,26 @@ test('login', async () => {
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
   expect(password).toBeTruthy();
+});
+
+test('register', async () => {
+    const newUser = testUser;
+    newUser.name = randomName();
+    const registerRes = await request(app).post('/api/auth').send(newUser);
+    expect(registerRes.status).toBe(200);
+    expect(registerRes.body.user.name).toEqual(newUser.name);
+});
+
+test('register fail', async () => {
+  testUser.name = null;
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  expect(registerRes.status).toBe(400);
+});
+
+test('update user', async () => {
+    // TODO
+});
+
+test('logout', async () => {
+    // TODO
 });
