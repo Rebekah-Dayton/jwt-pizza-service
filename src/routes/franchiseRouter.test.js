d7f22007d04a35d33dea5e4109ab202b14e826a5
Rebekah-Dayton <bekah060603@gmail.com>
2024-10-01 18:44:34 -0600

    created create and get franchise tests

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -2,27 +2,47 @@ const request = require('supertest');
 const app = require('../service');
 const { Role, DB } = require('../database/database.js');
 
-const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a', roles: [{ role: Role.Admin }] };
 let testUserAuthToken;
+let testID;
 
 function randomName() {
     return Math.random().toString(36).substring(2, 12);
 }
 
-async function createAdminUser() {
-  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
-  user.name = randomName();
-  user.email = user.name + '@admin.com';
+async function createFranchise() {
+    const newFranchise = {name: randomName(), admins: [{email: "f@jwt.com"}]}
 
-  await DB.addUser(user);
-
-  user.password = 'toomanysecrets';
-  return user;
+    const createRes = await request(app).post('/api/franchise').set('Authorization', `Bearer ${testUserAuthToken}`).send(newFranchise);    
+    expect(createRes.status).toBe(200);
+    expect(createRes.body.name).toBe(newFranchise.name);
+    expect(createRes.body.admins[0].id).toBe(3);
 }
-
+  
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
-  testUserAuthToken = registerRes.body.token;
+  await DB.addUser(testUser);
+  testUser.password = 'a';
+
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+
+  testUserAuthToken = loginRes.body.token;
+  testID = loginRes.body.user.id;
+  testUser.id = testID;
   expect(testUserAuthToken).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 });
+
+test('get franchises', async () => {
+    const getFranchisesRes = await request(app).get('/api/franchise');
+    expect(getFranchisesRes.status).toBe(200);
+    expect(getFranchisesRes.body).toBeDefined();
+});
+
+test('get user franchises', async () => {
+    await createFranchise();
+    const getUserFranchisesRes = await request(app).get(`/api/franchise/3`).set('Authorization', `Bearer ${testUserAuthToken}`);
+    expect(getUserFranchisesRes.status).toBe(200);
+    expect(getUserFranchisesRes.body[0].admins[0].id).toBe(3);
+});
+
+test('create franchise', createFranchise);
